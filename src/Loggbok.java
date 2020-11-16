96c92e1d2927aab88a50734652b30bce0f3e4459
alexander.holmstro3 <alle020916@gmail.com>
2020-11-16 11:01:59 +0100

    Fixat filhantering

--- src/Loggbok.java
+++ src/Loggbok.java
@@ -1,7 +1,11 @@
-public class Loggbok {
+import java.io.*;
+
+    public class Loggbok {
     private String time;
     private String text;
     private String log;
+    private String data[];
+    private String textFile = "data.txt";
 
     public Loggbok(String time, String text, String log) {
         this.time = time;
@@ -34,5 +38,28 @@ public class Loggbok {
         return "Loggbok{" +
                 "log='" + log + '\'' +
                 '}';
+    }
+    private void CharStreamExample() throws IOException {
+        PrintWriter out = null;
+        try {
+            out = new PrintWriter(new BufferedWriter(new FileWriter(textFile)));
+            for (int i = 0; i < data.length ; i ++) {
+                out.println(data[i]);
+            }
+        } finally {
+            out.close();
+        }
+
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new FileReader(textFile));
+            System.out.println("Reading from textfile " + textFile);
+            for (int i = 0 ; i < data.length ; i++) {
+                double d = Double.parseDouble(in.readLine());
+                System.out.println(d);
+            }
+        } finally {
+            in.close();
+        }
     }
 }
